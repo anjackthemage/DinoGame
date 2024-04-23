52971579ec99f1156ecaf53f4da6810497e95437
John Majernik <jdmajernik@gmail.com>
2024-04-22 20:51:15 -0400

    - Adding some default classes - Moving existing classes out of root source folder - Adding some editor themes - Limiting Target platforms to PC only, as mobile development seems unlikely

---
+++ Source/DinoGame/Core/DinoAssetManager.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "DinoAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DINOGAME_API UDinoAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+};
